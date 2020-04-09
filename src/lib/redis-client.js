be53caf9812201e721f57ae7f7110c9f3036de4b
fmvilas <fmvilas@gmail.com>
2020-04-09 21:10:23 +0200

    Leave redis password but ignore it when null

--- src/lib/redis-client.js
+++ src/lib/redis-client.js
@@ -5,7 +5,7 @@ module.exports.create = () => {
   const redisClient = redis.createClient({
     host: config.session.host,
     port: config.session.port,
-    password: config.session.password,
+    password: config.session.password || undefined,
     db: 1,
   });
   redisClient.unref();
