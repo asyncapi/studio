9e1540a99948d36478b95c0b040d83a162679d8a
AceTheCreator <devaze007@gmail.com>
2021-04-27 12:49:11 +0100

    changed app component to functional component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,40 +1,35 @@
 import React from 'react'
-import App from 'next/app'
 import AppContext from '../contexts/AppContext'
 import '../css/tailwind.css'
 
-class AsyncApiStudio extends App {
-  static async getInitialProps({ ctx }) {
-    if (!ctx || !ctx.req) return {}
+function AsyncApiStudio({ Component, pageProps, context }) {
+  return (
+    <AppContext.Provider value={context || {}}>
+      <Component {...pageProps} />
+    </AppContext.Provider>
+  );
+}
 
-    let user;
+AsyncApiStudio.getInitialProps = async ({ctx}) => {
+      if (!ctx || !ctx.req) return {};
 
-    if (ctx.req.user && ctx.req.user.id) {
-      user = await ctx.req.studio.users.getUserPublicInfo(ctx.req.user.id)
-    }
+      let user;
 
-    return {
-      context: {
-        user,
-        url: {
-          full: ctx.req.url,
-          query: ctx.req.query,
-          path: ctx.req.path,
+      if (ctx.req.user && ctx.req.user.id) {
+        user = await ctx.req.studio.users.getUserPublicInfo(ctx.req.user.id);
+      }
+
+      return {
+        context: {
+          user,
+          url: {
+            full: ctx.req.url,
+            query: ctx.req.query,
+            path: ctx.req.path,
+          },
+          ui: ctx.req.studio.ui,
         },
-        ui: ctx.req.studio.ui,
-      },
-    }
-  }
-
-  render() {
-    const { Component, pageProps, context } = this.props
-
-    return (
-      <AppContext.Provider value={context || {}}>
-        <Component {...pageProps} />
-      </AppContext.Provider>
-    )
-  }
+      };
 }
 
 export default AsyncApiStudio
