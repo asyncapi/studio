70d4d846d2d1dd896a542d946a41685dc0fbc9b0
Matatjahu <urbanczyk.maciej.95@gmail.com>
2021-05-18 18:25:55 +0200

    update deps

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,10 +1,12 @@
 import { useEffect } from 'react';
 import dynamic from 'next/dynamic';
+import "@asyncapi/react-component/styles/default.min.css";
+
+const AsyncAPIUI = dynamic(() => import('@asyncapi/react-component/browser'), { ssr: false });
 
 export default function Preview ({ code, onError = () => {}, onContentChange = () => {} }) {
   // Render on the browser only
   if (typeof navigator === 'undefined') return null;
-  const AsyncAPIUI = dynamic(() => import('@asyncapi/react-component/bundles/umd'));
 
   useEffect(() => {
     onContentChange();
