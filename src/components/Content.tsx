ac613375a94acb07df9646b37b4faee3de99378c
Matatjahu <urbanczyk.maciej.95@gmail.com>
2021-09-30 10:25:42 +0200

    chore: add navigation

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,15 +1,65 @@
 import React from 'react';
+import SplitPane from 'react-split-pane';
 
 import { Editor } from './Editor/Editor';
+import { Navigation } from './Navigation';
 import { Template } from './Template';
+
+import { debounce } from '../helpers';
+import state from '../state';
 
 interface ContentProps {}
 
 export const Content: React.FunctionComponent<ContentProps> = () => {
+  const sidebarState = state.useSidebarState();
+
+  const navigationEnabled = sidebarState.panels.navigation.get();
+  const editorEnabled = sidebarState.panels.editor.get();
+  const templateEnabled = sidebarState.panels.template.get();
+
+  const navigationAndEditor = (
+    <SplitPane
+      minSize={220}
+      maxSize={360}
+      pane1Style={!navigationEnabled ? { width: '0px' } : { overflow: 'auto' }}
+      pane2Style={!editorEnabled ? { width: '0px' } : undefined}
+      primary={!editorEnabled ? 'second' : 'first'}
+      defaultSize={
+        parseInt(localStorage.getItem('splitPos:left') || '0', 10) || 220
+      }
+      onChange={debounce((size: string) => {
+        localStorage.setItem('splitPos:left', String(size));
+      }, 100)}
+    >
+      <Navigation />
+      <Editor />
+    </SplitPane>
+  );
+
   return (
     <div className="flex flex-1 flex-row relative">
-      <Editor />
-      <Template />
+      <div className="flex flex-1 flex-row relative">
+        <SplitPane
+          minSize={0}
+          pane1Style={
+            !navigationEnabled && !editorEnabled ? { width: '0px' } : undefined
+          }
+          pane2Style={
+            !templateEnabled ? { width: '0px' } : { overflow: 'auto' }
+          }
+          primary={!templateEnabled ? 'second' : 'first'}
+          defaultSize={
+            parseInt(localStorage.getItem('splitPos:center') || '0', 10) ||
+            '55%'
+          }
+          onChange={debounce((size: string) => {
+            localStorage.setItem('splitPos:center', String(size));
+          }, 100)}
+        >
+          {navigationAndEditor}
+          <Template />
+        </SplitPane>
+      </div>
     </div>
   );
 };
--- src/studio.tsx
+++ src/studio.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
 
-import { Content, Toolbar } from './components';
+import { Content, Sidebar, Toolbar } from './components';
 
 export interface AsyncAPIStudioProps {}
 
@@ -10,6 +10,7 @@ const AsyncAPIStudio: React.FunctionComponent<AsyncAPIStudioProps> = () => {
     <div className="flex flex-col h-full w-full h-screen">
       <Toolbar />
       <div className="flex flex-row flex-1 overflow-hidden">
+        <Sidebar />
         <Content />
       </div>
       <Toaster position="bottom-center" reverseOrder={false} />
