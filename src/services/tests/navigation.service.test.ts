b3e99bd213c046585b2824de22217bed8eac649b
Matatjahu <urbanczyk.maciej.95@gmail.com>
2021-10-07 12:53:36 +0200

    udpate tests

--- src/services/tests/navigation.service.test.ts
+++ src/services/tests/navigation.service.test.ts
@@ -3,115 +3,62 @@ import { NavigationService } from '../navigation.service';
 describe('NavigationService', () => {
   describe('.isReadOnly', () => {
     test('should return false if reaOnly flag is not defined', () => {
-      const location = {
-        ...window.location,
-        search: '?url=some-url.json',
-      };
-      Object.defineProperty(window, 'location', {
-        writable: true,
-        value: location,
-      });
-
+      updateLocation('?url=some-url.json');
       const result = NavigationService.isReadOnly();
       expect(result).toEqual(false);
     });
 
     test('should return true if reaOnly flag is defined - empty value case', () => {
-      const location = {
-        ...window.location,
-        search: '?readOnly',
-      };
-      Object.defineProperty(window, 'location', {
-        writable: true,
-        value: location,
-      });
-
+      updateLocation('?readOnly');
       const result = NavigationService.isReadOnly();
       expect(result).toEqual(true);
     });
 
     test('should return true if reaOnly flag is defined - true value case', () => {
-      const location = {
-        ...window.location,
-        search: '?readOnly=true',
-      };
-      Object.defineProperty(window, 'location', {
-        writable: true,
-        value: location,
-      });
-
+      updateLocation('?readOnly=true');
       const result = NavigationService.isReadOnly();
       expect(result).toEqual(true);
     });
 
     test('should return false if reaOnly flag is not defined - non empty/true value case', () => {
-      const location = {
-        ...window.location,
-        search: '?readOnly=false',
-      };
-      Object.defineProperty(window, 'location', {
-        writable: true,
-        value: location,
-      });
-
+      updateLocation('?readOnly=false');
       const result = NavigationService.isReadOnly();
       expect(result).toEqual(false);
     });
 
     test('should return false if reaOnly flag is not defined - strict mode case without other parameters', () => {
-      const location = {
-        ...window.location,
-        search: '?readOnly=true',
-      };
-      Object.defineProperty(window, 'location', {
-        writable: true,
-        value: location,
-      });
-
+      updateLocation('?readOnly=true');
       const result = NavigationService.isReadOnly(true);
       expect(result).toEqual(false);
     });
 
     test('should return true if reaOnly flag is not defined - strict mode case with url parameter', () => {
-      const location = {
-        ...window.location,
-        search: '?readOnly=true&url=some-url.json',
-      };
-      Object.defineProperty(window, 'location', {
-        writable: true,
-        value: location,
-      });
-
+      updateLocation('?readOnly=true&url=some-url.json');
       const result = NavigationService.isReadOnly(true);
       expect(result).toEqual(true);
     });
 
     test('should return true if reaOnly flag is not defined - strict mode case with load parameter', () => {
-      const location = {
-        ...window.location,
-        search: '?readOnly=true&load=some-url.json',
-      };
-      Object.defineProperty(window, 'location', {
-        writable: true,
-        value: location,
-      });
-
+      updateLocation('?readOnly=true&load=some-url.json');
       const result = NavigationService.isReadOnly(true);
       expect(result).toEqual(true);
     });
 
     test('should return true if reaOnly flag is not defined - strict mode case with base64 parameter', () => {
-      const location = {
-        ...window.location,
-        search: '?readOnly=true&base64=AsyncAPI',
-      };
-      Object.defineProperty(window, 'location', {
-        writable: true,
-        value: location,
-      });
-
+      updateLocation('?readOnly=true&base64=AsyncAPI');
       const result = NavigationService.isReadOnly(true);
       expect(result).toEqual(true);
     });
   });
 });
+
+function updateLocation(search: string) {
+  const location = {
+    ...window.location,
+    search,
+  };
+  Object.defineProperty(window, 'location', {
+    writable: true,
+    value: location,
+  });
+}
