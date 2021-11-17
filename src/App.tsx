97187f1340964bb132efce23d51331f0adea3ec9
Matatjahu <urbanczyk.maciej.95@gmail.com>
2021-11-17 11:00:40 +0100

    feat: add splash (loading) screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import AsyncAPIStudio from './studio';
 
 const App: React.FunctionComponent = () => {
+  useEffect(() => {
+    const preloader = document.getElementsByClassName("preloader").item(0);
+    if (preloader) {
+      preloader.classList.add('loaded');
+    }
+  }, []);
+
   return (
     <AsyncAPIStudio />
   );
