eaf712229ede6821e2f42a3725db31db68f69c56
Matatjahu <urbanczyk.maciej.95@gmail.com>
2021-11-17 16:45:08 +0100

    improve hiding of splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import React, { useEffect } from 'react';
 
 import AsyncAPIStudio from './studio';
 
+import state from './state';
+
 const App: React.FunctionComponent = () => {
+  const editorState = state.useEditorState();
+  const editorLoaded = editorState.editorLoaded.get();
+
   useEffect(() => {
-    const preloader = document.getElementsByClassName('preloader').item(0);
-    if (preloader) {
-      preloader.classList.add('loaded');
+    if (editorLoaded === true) {
+      const preloader = document.getElementsByClassName('preloader').item(0);
+      if (preloader) {
+        preloader.classList.add('loaded');
+      }
     }
-  }, []);
+  }, [editorLoaded]); // eslint-disable-line
 
   return (
     <AsyncAPIStudio />
