520acf9212e6ab76df1ba46762dd019ba25f3bfe
Matatjahu <urbanczyk.maciej.95@gmail.com>
2021-12-14 16:40:07 +0100

    fix sonarcloud errors

--- src/components/common/Switch.tsx
+++ src/components/common/Switch.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 interface SwitchProps {
   toggle?: boolean;
@@ -10,7 +10,6 @@ export const Switch: React.FunctionComponent<SwitchProps> = ({
   onChange,
 }) => {
   const [toggle, setToggle] = useState(initToggle);
-  const toggleClass = ' transform translate-x-5 bg-pink-500';
 
   return (
     <div 
@@ -29,7 +28,6 @@ export const Switch: React.FunctionComponent<SwitchProps> = ({
           <div className={`w-12 h-6 rounded-full shadow-inner p-1 ${toggle ? 'bg-pink-500' : 'bg-gray-300'}`}>
             <div className={`w-6 h-4 rounded-full bg-white transition ${toggle ? 'transform translate-x-4' : ''}`}></div>
           </div>
-          {/* <div className={`absolute w-6 h-6 bg-white rounded-full bg-gray-200 transition ${toggle ? toggleClass : null}`} /> */}
         </div>
       </label>
     </div>
