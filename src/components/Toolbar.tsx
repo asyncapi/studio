0cc3a7520aa4286546b5e64c0a51e6800ead7fdc
Namya LG <53875297+Namyalg@users.noreply.github.com>
2022-04-06 21:29:15 +0530

    refactor: change logo of the AsyncAPI Studio, update OG image and icons (#291)
    Co-authored-by: Matatjahu <urbanczyk.maciej.95@gmail.com>

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -6,24 +6,21 @@ interface ToolbarProps {}
 export const Toolbar: React.FunctionComponent<ToolbarProps> = () => {
   return (
     <div>
-      <div className="px-4 border-b border-gray-700 bg-gray-800">
-        <div className="flex items-center justify-between h-16 px-4">
+      <div className="px-2 border-b border-gray-700 bg-gray-800">
+        <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <div className="flex-shrink-0">
+            <div className="flex-shrink-0 ml-1.5">
               <img
-                className="inline-block h-20"
-                src={`${process.env.PUBLIC_URL}/img/logo-horizontal-white.svg`}
+                className="inline-block h-16"
+                src={`${process.env.PUBLIC_URL}/img/logo-studio.svg`}
                 alt="AsyncAPI Logo"
               />
-              <span className="inline-block text-xl text-pink-500 font-normal italic tracking-wide -ml-1 transform translate-y-0.5">
-                studio
-              </span>
-              <span className="inline-block text-xs text-teal-500 font-normal tracking-wide ml-0.5 transform translate-y-0.5">
+              <span className="inline-block text-xs text-teal-500 font-normal ml-1 tracking-wider uppercase" style={{ transform: 'translateY(0.3125rem)' }}>
                 beta
               </span>
             </div>
           </div>
-          <ul className="flex items-center text-pink-500">
+          <ul className="flex items-center text-pink-500 mr-2">
             <li className="text-xl opacity-75 hover:opacity-100">
               <a href='https://asyncapi.com' title='AsyncAPI Website' target='_blank' rel="noreferrer">
                 <IoGlobeOutline />
