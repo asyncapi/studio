a5ea5e9c27f2bfa5787ef5e014946eccda77525a
Matatjahu <urbanczyk.maciej.95@gmail.com>
2022-04-06 13:07:01 +0200

    update logos

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -6,21 +6,21 @@ interface ToolbarProps {}
 export const Toolbar: React.FunctionComponent<ToolbarProps> = () => {
   return (
     <div>
-      <div className="px-4 border-b border-gray-700 bg-gray-800">
-        <div className="flex items-center justify-between h-16 px-4">
+      <div className="px-2 border-b border-gray-700 bg-gray-800">
+        <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <div className="flex-shrink-0">
+            <div className="flex-shrink-0 ml-1.5">
               <img
-                className="inline-block h-20"
+                className="inline-block h-16"
                 src={`${process.env.PUBLIC_URL}/img/logo-studio.svg`}
                 alt="AsyncAPI Logo"
               />
-              <span className="inline-block text-xs text-teal-500 font-normal tracking-wide ml-0.5 transform translate-y-0.5">
+              <span className="inline-block text-xs text-teal-500 font-normal tracking-wider uppercase" style={{ transform: "translateY(0.3125rem)" }}>
                 beta
               </span>
             </div>
           </div>
-          <ul className="flex items-center text-pink-500">
+          <ul className="flex items-center text-pink-500 mr-2">
             <li className="text-xl opacity-75 hover:opacity-100">
               <a href='https://asyncapi.com' title='AsyncAPI Website' target='_blank' rel="noreferrer">
                 <IoGlobeOutline />
