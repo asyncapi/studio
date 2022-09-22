47602b54712b269615b53dd4fcea889d67349b69
Viacheslav Turovskyi <aeworxet@protonmail.com>
2022-09-22 15:48:22 +0300

    fix: force 'crypto.createHash()' to use SHA-256 (#360) (#428)

--- craco.config.js
+++ craco.config.js
@@ -1,3 +1,8 @@
+// eslint-disable-next-line
+const crypto = require('crypto');         // Force method use SHA-256 to address
+const cryptCreateHashOrig = crypto.createHash;     // OpenSSL 3.0 deprecation of
+crypto.createHash = () => cryptCreateHashOrig('sha256');        // MD4 algorithm
+
 module.exports = {
   style: {
     postcss: {
