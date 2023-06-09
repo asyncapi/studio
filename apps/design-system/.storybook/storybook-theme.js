4cf87d91cbdf77a82dcd489b954ba8aaa09d272e
Fran Mendez <fmvilas@gmail.com>
2023-06-09 16:56:47 +0200

    Change theme title and url

--- apps/design-system/.storybook/storybook-theme.js
+++ apps/design-system/.storybook/storybook-theme.js
@@ -3,7 +3,7 @@ import { create } from '@storybook/theming';
 import brandImage from './logo.png';
 
 export default create({
-  brandTitle: 'AsyncAPI Design System',
-  brandUrl: 'https://asyncapi.com/',
+  brandTitle: 'AsyncAPI Studio Design System',
+  brandUrl: 'https://studio.asyncapi.com/',
   brandImage,
 });
