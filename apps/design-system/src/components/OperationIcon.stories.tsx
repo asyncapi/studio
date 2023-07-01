e7bc50f1084fd7665fc52165bf124a47b3be1e71
Prince Rajpoot <prince.rajpoot.20@gmail.com>
2023-07-01 19:32:35 +0530

    fix: made changes

--- apps/design-system/src/components/OperationIcon.stories.tsx
+++ apps/design-system/src/components/OperationIcon.stories.tsx
@@ -13,6 +13,7 @@ export const SendIcon = () => <OperationIcon operation="send" className="w-7 h-7
   component: OperationIcon,
 };
 
+<<<<<<< HEAD
 export const WithReplyIcon = (args) => <OperationIcon {...args} />;
 WithReplyIcon.args = {
   type: 'withReply',
@@ -31,3 +32,8 @@ SendIcon.args = {
   className: 'fill-current text-gray-500',
 };
 >>>>>>> d02c281 (feat: initial draft for operation icon)
+=======
+export const WithReplyIcon = () => <OperationIcon operation="reply" className="w-7 h-7 fill-current text-gray-500" />;
+export const ReceiveIcon = () => <OperationIcon operation="receive" className="w-7 h-7 fill-current text-gray-500" />;
+export const SendIcon = () => <OperationIcon operation="send" className="w-7 h-7 fill-current text-gray-500" />;
+>>>>>>> 1061258 (fix: made changes)
