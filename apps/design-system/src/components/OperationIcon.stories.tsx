db8bbb1578bf82bbff0e394440c2f2c3406dfde5
Prince Rajpoot <prince.rajpoot.20@gmail.com>
2023-07-02 01:40:20 +0530

    fix: remove title

--- apps/design-system/src/components/OperationIcon.stories.tsx
+++ apps/design-system/src/components/OperationIcon.stories.tsx
@@ -1,10 +1,9 @@
 import { OperationIcon } from 'ui';
 
 export default {
-    title: 'Components/OperationIcon',
     component: OperationIcon,
 };
 
-export const WithReplyIcon = () => <OperationIcon operation="reply" className="w-7 h-7 fill-current text-gray-500" />;
-export const ReceiveIcon = () => <OperationIcon operation="receive" className="w-7 h-7 fill-current text-gray-500" />;
-export const SendIcon = () => <OperationIcon operation="send" className="w-7 h-7 fill-current text-gray-500" />;
+export const WithReplyIcon = () => <OperationIcon operation="reply" className="w-7 h-7" />;
+export const ReceiveIcon = () => <OperationIcon operation="receive" className="w-7 h-7" />;
+export const SendIcon = () => <OperationIcon operation="send" className="w-7 h-7" />;
