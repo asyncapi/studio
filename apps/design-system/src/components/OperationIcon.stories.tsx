c4ff9a5374c3737877c3dcb91e7415974811f558
Prince Rajpoot <prince.rajpoot.20@gmail.com>
2023-07-01 16:39:20 +0530

    feat: initial draft for operation icon

--- apps/design-system/src/components/OperationIcon.stories.tsx
+++ apps/design-system/src/components/OperationIcon.stories.tsx
@@ -1,9 +1,33 @@
 import { OperationIcon } from 'ui';
 
 export default {
+<<<<<<< HEAD
     component: OperationIcon,
 };
 
 export const WithReplyIcon = () => <OperationIcon operation="reply" className="w-7 h-7" />;
 export const ReceiveIcon = () => <OperationIcon operation="receive" className="w-7 h-7" />;
 export const SendIcon = () => <OperationIcon operation="send" className="w-7 h-7" />;
+=======
+  title: 'OperationIcon',
+  component: OperationIcon,
+};
+
+export const WithReplyIcon = (args) => <OperationIcon {...args} />;
+WithReplyIcon.args = {
+  type: 'withReply',
+  className: 'fill-current text-gray-500',
+};
+
+export const ReceiveIcon = (args) => <OperationIcon {...args} />;
+ReceiveIcon.args = {
+  type: 'receive',
+  className: 'fill-current text-gray-500',
+};
+
+export const SendIcon = (args) => <OperationIcon {...args} />;
+SendIcon.args = {
+  type: 'send',
+  className: 'fill-current text-gray-500',
+};
+>>>>>>> d02c281 (feat: initial draft for operation icon)
