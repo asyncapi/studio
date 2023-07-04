b494305d2d9ed03fe0f453dfcb69c825ff72df3f
Prince Rajpoot <44585452+princerajpoot20@users.noreply.github.com>
2023-07-05 00:09:44 +0530

    fix: update default background to dark of operation icon (#718)
    Co-authored-by: Fran Méndez <fmvilas@gmail.com>

--- apps/design-system/src/components/OperationIcon.stories.tsx
+++ apps/design-system/src/components/OperationIcon.stories.tsx
@@ -2,8 +2,13 @@ import { OperationIcon } from 'ui';
 
 export default {
     component: OperationIcon,
+    parameters: {
+        backgrounds: {
+          default: 'dark'
+        }
+    }
 };
 
-export const WithReplyIcon = () => <OperationIcon operation="reply" className="w-7 h-7" />;
-export const ReceiveIcon = () => <OperationIcon operation="receive" className="w-7 h-7" />;
-export const SendIcon = () => <OperationIcon operation="send" className="w-7 h-7" />;
+export const WithReplyIcon = () => <OperationIcon operation="reply" />;
+export const ReceiveIcon = () => <OperationIcon operation="receive" />;
+export const SendIcon = () => <OperationIcon operation="send" />;
