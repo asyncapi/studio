0a0058b5a90a6ce81c9e802ad1ba699d4eead019
Fran Méndez <fmvilas@gmail.com>
2023-07-04 17:23:14 +0200

    feat: add ProtocolBadge component (#712)

---
+++ apps/design-system/src/components/ProtocolBadge.stories.tsx
@@ -0,0 +1,95 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { ProtocolBadge } from 'ui'
+
+export default {
+  component: ProtocolBadge,
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+}
+
+export const HTTP = {
+  args: {
+    protocol: 'http'
+  }
+}
+
+export const Kafka = {
+  args: {
+    protocol: 'kafka'
+  }
+}
+
+export const Websocket = {
+  args: {
+    protocol: 'websocket'
+  }
+}
+
+export const AMQP = {
+  args: {
+    protocol: 'amqp'
+  }
+}
+
+export const MQTT = {
+  args: {
+    protocol: 'mqtt'
+  }
+}
+
+export const GooglePubSub = {
+  args: {
+    protocol: 'googlepubsub'
+  }
+}
+
+export const IBMMQ = {
+  args: {
+    protocol: 'ibmmq'
+  }
+}
+
+export const NATS = {
+  args: {
+    protocol: 'nats'
+  }
+}
+
+export const Pulsar = {
+  args: {
+    protocol: 'pulsar'
+  }
+}
+
+export const Redis = {
+  args: {
+    protocol: 'redis'
+  }
+}
+
+export const SNS = {
+  args: {
+    protocol: 'sns'
+  }
+}
+
+export const SQS = {
+  args: {
+    protocol: 'sqs'
+  }
+}
+
+export const Solace = {
+  args: {
+    protocol: 'solace'
+  }
+}
+
+export const STOMP = {
+  args: {
+    protocol: 'stomp'
+  }
+}
