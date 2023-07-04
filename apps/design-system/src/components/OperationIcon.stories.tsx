19173917a57610b53d47a0202528928180eab51d
Prince Rajpoot <prince.rajpoot.20@gmail.com>
2023-07-04 23:23:45 +0530

    fix operationicon-stories accordingly

--- apps/design-system/src/components/OperationIcon.stories.tsx
+++ apps/design-system/src/components/OperationIcon.stories.tsx
@@ -9,6 +9,6 @@ export default {
     }
 };
 
-export const WithReplyIcon = () => <OperationIcon operation="reply" className="w-7 h-7" />;
-export const ReceiveIcon = () => <OperationIcon operation="receive" className="w-7 h-7" />;
-export const SendIcon = () => <OperationIcon operation="send" className="w-7 h-7" />;
+export const WithReplyIcon = () => <OperationIcon operation="reply" />;
+export const ReceiveIcon = () => <OperationIcon operation="receive" />;
+export const SendIcon = () => <OperationIcon operation="send" />;
