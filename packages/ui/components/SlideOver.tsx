83736f574d0ab2146c9fbaeede76bdc995e44e6d
Prince Rajpoot <44585452+princerajpoot20@users.noreply.github.com>
2023-07-04 13:07:41 +0530

    Update packages/ui/components/SlideOver.tsx
    Co-authored-by: Fran Méndez <fmvilas@gmail.com>

--- packages/ui/components/SlideOver.tsx
+++ packages/ui/components/SlideOver.tsx
@@ -17,7 +17,7 @@ export const SlideOver: React.FC<SlideOverProps> = ({ isOpen, onClose, children 
   };
 
   return (
-    <div className="fixed right-0 top-0 bottom-0 w-full max-w-lg h-full bg-gray-950/[.67] text-gray-200 shadow-lg overflow-auto p-2.5 border-l-2 border-gray-700 backdrop-blur-[20px]" tabIndex={0} onKeyDown={handleKeyDown}>
+    <div className="fixed right-0 top-0 bottom-0 w-full max-w-lg h-full bg-gray-950/[.67] text-gray-200 shadow-lg overflow-auto p-8 border-l-2 border-gray-700 backdrop-blur-[20px]" tabIndex={0} onKeyDown={handleKeyDown}>
         <button className="absolute top-2 right-2 focus:outline-white" onClick={onClose} aria-label="Close">
             <XMarkIcon className="h-6 w-6"/> 
         </button>
