d6ddfcf4ed5fc1911637a84ca550ce492e53bbca
Khuda Dad Nomani <khudadad414@gmail.com>
2023-09-05 09:47:09 +0100

    revert styles

--- packages/ui/components/DropdownMenu.tsx
+++ packages/ui/components/DropdownMenu.tsx
@@ -25,22 +25,31 @@ interface DropdownMenuItemComponentProps {
 }
 
 const DropdownMenuItemComponent: FunctionComponent<DropdownMenuItemComponentProps> = ({ item }) => {
-  return item.type === 'separator' ? (
-    <RadixDropdownMenu.Separator className='w-full h-px bg-gray-700 my-2' />
-  ) : (
-    <RadixDropdownMenu.Item
-      className='text-gray-200 text-sm leading-7 px-2.5 cursor-pointer rounded outline-none select-none hover:bg-gray-700 focus:bg-gray-700'
-      onSelect={item.onSelect}
-    >
-      {item.title}
-    </RadixDropdownMenu.Item>
+  return (
+    item.type === 'separator' ? (
+      <RadixDropdownMenu.Separator className="w-full h-px bg-gray-700 my-2" />
+    ) : (
+      <RadixDropdownMenu.Item
+        className="text-gray-200 text-sm leading-7 px-2.5 cursor-pointer rounded outline-none select-none hover:bg-gray-700 focus:bg-gray-700"
+        onSelect={item.onSelect}
+      >
+        {item.title}
+      </RadixDropdownMenu.Item>
+    )
   )
 }
 
-export const DropdownMenu: FunctionComponent<DropdownMenuProps> = ({ trigger, items, side, align }) => {
+export const DropdownMenu: FunctionComponent<DropdownMenuProps> = ({
+  trigger,
+  items,
+  side,
+  align,
+}) => {
   return (
     <RadixDropdownMenu.Root>
-      <RadixDropdownMenu.Trigger asChild>{trigger}</RadixDropdownMenu.Trigger>
+      <RadixDropdownMenu.Trigger asChild>
+        {trigger}
+      </RadixDropdownMenu.Trigger>
       <RadixDropdownMenu.Portal>
         <RadixDropdownMenu.Content className="min-w-[220px] bg-gray-950 rounded-md p-2.5 shadow" sideOffset={5} side={side} align={align}>
           {
--- packages/ui/components/index.tsx
+++ packages/ui/components/index.tsx
@@ -1,16 +1,17 @@
 // styles
-import "./styles.css"
+import './styles.css'
 
 // components
-export * from "./DropdownMenu"
-export * from "./Logo"
-export * from "./Modal"
-export * from "./OperationIcon"
-export * from "./ProtocolBadge"
-export * from "./Sidebar"
-export * from "./SlideOver"
-export * from "./Toolbar"
-export * from "./EditorSwitch"
-export * from "./Form"
-export * from "./IconButton"
-export { default as Tooltip } from "./Tooltip"
+export * from './EditorSwitch'
+export * from './DropdownMenu'
+export * from './Logo'
+export * from './Modal'
+export * from './OperationIcon'
+export * from './ProtocolBadge'
+export * from './Sidebar'
+export * from './SlideOver'
+export * from './Toolbar'
+export * from './EditorSwitch'
+export * from './Form'
+export * from './IconButton'
+export { default as Tooltip } from './Tooltip'
