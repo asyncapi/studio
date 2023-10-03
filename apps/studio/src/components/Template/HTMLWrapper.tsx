1980104fa2922b8a4dbe48d465564709ca03fb27
jonas-lt@live.dk <jonas-lt@live.dk>
2023-10-03 11:37:55 +0200

    switch to new react components

--- apps/studio/src/components/Template/HTMLWrapper.tsx
+++ apps/studio/src/components/Template/HTMLWrapper.tsx
@@ -4,12 +4,12 @@ import { AsyncApiComponentWP } from '@asyncapi/react-component';
 import { useServices } from '../../services';
 import { appState, useDocumentsState, useSettingsState, useOtherState, otherState } from '../../state';
 
-import { OldAsyncAPIDocument as AsyncAPIDocument, convertToOldAPI } from '@asyncapi/parser/cjs';
+import { AsyncAPIDocumentInterface } from '@asyncapi/parser/cjs';
 
 interface HTMLWrapperProps {}
 
 export const HTMLWrapper: React.FunctionComponent<HTMLWrapperProps> = () => {
-  const [parsedSpec, setParsedSpec] = useState<AsyncAPIDocument | null>(null);
+  const [parsedSpec, setParsedSpec] = useState<AsyncAPIDocumentInterface | null>(null);
   const { navigationSvc } = useServices();
   const document = useDocumentsState(state => state.documents['asyncapi']?.document) || null;
 
@@ -22,15 +22,13 @@ export const HTMLWrapper: React.FunctionComponent<HTMLWrapperProps> = () => {
 
   useEffect(() => {
     if (autoRendering || parsedSpec === null) {
-      const oldDocument = document !== null ? convertToOldAPI(document) : null;
-      setParsedSpec(oldDocument);
+      setParsedSpec(document);
     }
   }, [document]); // eslint-disable-line
 
   useEffect(() => {
     if (templateRerender) {
-      const oldDocument = document !== null ? convertToOldAPI(document) : null;
-      setParsedSpec(oldDocument);
+      setParsedSpec(document);
       otherState.setState({ templateRerender: false });
     }
   }, [templateRerender]); // eslint-disable-line
@@ -39,14 +37,6 @@ export const HTMLWrapper: React.FunctionComponent<HTMLWrapperProps> = () => {
     return (
       <div className="flex flex-1 overflow-hidden h-full justify-center items-center text-2xl mx-auto px-6 text-center">
         <p>Empty or invalid document. Please fix errors/define AsyncAPI document.</p>
-      </div>
-    );
-  }
-
-  if (document?.version() === '3.0.0') {
-    return (
-      <div className="flex flex-1 overflow-hidden h-full justify-center items-center text-2xl mx-auto px-6 text-center">
-        <p>Documentation preview is not supported yet for v3.0.0 specifications.</p>
       </div>
     );
   }
