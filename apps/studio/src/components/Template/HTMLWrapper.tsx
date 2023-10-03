1e531a63e90b93c37c0f6d3c5ff77b6d3bbbba7d
jonas-lt@live.dk <jonas-lt@live.dk>
2023-10-03 09:39:43 +0200

    update html template

--- apps/studio/src/components/Template/HTMLWrapper.tsx
+++ apps/studio/src/components/Template/HTMLWrapper.tsx
@@ -4,14 +4,15 @@ import { AsyncApiComponentWP } from '@asyncapi/react-component';
 import { useServices } from '../../services';
 import { appState, useDocumentsState, useSettingsState, useOtherState, otherState } from '../../state';
 
-import type { AsyncAPIDocumentInterface } from '@asyncapi/parser/cjs';
+import { OldAsyncAPIDocument as AsyncAPIDocument, convertToOldAPI } from '@asyncapi/parser/cjs';
 
 interface HTMLWrapperProps {}
 
 export const HTMLWrapper: React.FunctionComponent<HTMLWrapperProps> = () => {
-  const [parsedSpec, setParsedSpec] = useState<AsyncAPIDocumentInterface | null>(null);
+  const [parsedSpec, setParsedSpec] = useState<AsyncAPIDocument | null>(null);
   const { navigationSvc } = useServices();
   const document = useDocumentsState(state => state.documents['asyncapi']?.document) || null;
+
   const autoRendering = useSettingsState(state => state.templates.autoRendering);
   const templateRerender = useOtherState(state => state.templateRerender);
 
@@ -21,13 +22,15 @@ export const HTMLWrapper: React.FunctionComponent<HTMLWrapperProps> = () => {
 
   useEffect(() => {
     if (autoRendering || parsedSpec === null) {
-      setParsedSpec(document);
+      const oldDocument = document !== null ? convertToOldAPI(document) : null;
+      setParsedSpec(oldDocument);
     }
   }, [document]); // eslint-disable-line
 
   useEffect(() => {
     if (templateRerender) {
-      setParsedSpec(document);
+      const oldDocument = document !== null ? convertToOldAPI(document) : null;
+      setParsedSpec(oldDocument);
       otherState.setState({ templateRerender: false });
     }
   }, [templateRerender]); // eslint-disable-line
@@ -36,6 +39,14 @@ export const HTMLWrapper: React.FunctionComponent<HTMLWrapperProps> = () => {
     return (
       <div className="flex flex-1 overflow-hidden h-full justify-center items-center text-2xl mx-auto px-6 text-center">
         <p>Empty or invalid document. Please fix errors/define AsyncAPI document.</p>
+      </div>
+    );
+  }
+
+  if (document?.version() === '3.0.0') {
+    return (
+      <div className="flex flex-1 overflow-hidden h-full justify-center items-center text-2xl mx-auto px-6 text-center">
+        <p>Documentation preview is not supported yet for v3.0.0 specifications.</p>
       </div>
     );
   }
