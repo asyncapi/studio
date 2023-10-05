7fd77f8ca73e0d7300ff327b682c123b11e1a579
Jonas Lagoni <jonas-lt@live.dk>
2023-10-05 12:38:13 +0200

    feat: enable v3 documentation rendering (#797)

--- apps/studio/src/components/Sidebar.tsx
+++ apps/studio/src/components/Sidebar.tsx
@@ -89,7 +89,7 @@ export const Sidebar: FunctionComponent<SidebarProps> = () => {
       onClick: () => updateState('secondaryPanel', 'template'),
       icon: <VscOpenPreview className="w-5 h-5" />,
       tooltip: 'HTML preview',
-      enabled: !isV3
+      enabled: true
     },
     // visuliser
     {
