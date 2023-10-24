58ce06414591b5a6a1dc904b639f45b97ee4d1d9
Khuda Dad Nomani <khudadad414@gmail.com>
2023-10-24 14:38:59 +0100

    add IconButton stories

---
+++ apps/design-system/src/components/IconButton.stories.tsx
@@ -0,0 +1,20 @@
+import { FunctionComponent } from 'react';
+import { Meta } from '@storybook/react';
+import { IconButton } from '@asyncapi/studio-ui'; 
+import { AddIcon } from '@asyncapi/studio-ui/icons'; 
+
+
+const meta: Meta<typeof IconButton> = {
+  component: IconButton,
+  parameters: {
+    layout: "fullscreen",
+    backgrounds: {
+      default: "dark",
+    },
+  },
+}
+export default meta;
+
+export const Default = () => <IconButton icon={AddIcon} />
+export const WithText = () => <IconButton icon={AddIcon} text="Click Me" />;
+export const WithClickEvent = () => <IconButton icon={AddIcon} onClick={() => alert('Button clicked!')} />
