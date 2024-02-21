aa7a39eb0bcb68cf5262828fbfd06fe3cdd2f204
Mohammed Riyan@123 <rk5098863@gmail.com>
2024-02-21 11:24:24 +0000

    added  Loading animation in studio

--- apps/studio/src/components/Template/HTMLWrapper.tsx
+++ apps/studio/src/components/Template/HTMLWrapper.tsx
@@ -12,6 +12,7 @@ export const HTMLWrapper: React.FunctionComponent<HTMLWrapperProps> = () => {
   const [parsedSpec, setParsedSpec] = useState<AsyncAPIDocumentInterface | null>(null);
   const { navigationSvc } = useServices();
   const document = useDocumentsState(state => state.documents['asyncapi']?.document) || null;
+  const[loading, setloading] = useState(false);
 
   const autoRendering = useSettingsState(state => state.templates.autoRendering);
   const templateRerender = useOtherState(state => state.templateRerender);
@@ -33,10 +34,24 @@ export const HTMLWrapper: React.FunctionComponent<HTMLWrapperProps> = () => {
     }
   }, [templateRerender]); // eslint-disable-line
 
+  useEffect(()=>{
+    if (!document){
+      setloading(true);
+      const timer = setTimeout(() => {
+        setloading(false);
+      }, 1000);
+      return () => clearTimeout(timer);
+    }
+  },[document])
   if (!document) {
     return (
       <div className="flex flex-1 overflow-hidden h-full justify-center items-center text-2xl mx-auto px-6 text-center">
-        <p>Empty or invalid document. Please fix errors/define AsyncAPI document.</p>
+        {loading ?(
+          <div className="rotating-wheel"></div>
+        ) : (
+          <p>Empty or invalid document. Please fix errors/define AsyncAPI document.</p>
+        )
+        }
       </div>
     );
   }
