736864a9bc34512d2fecff16ab561011a8ade58d
Mohit kumar singh <tihom4537@Mohits-MacBook-Air.local>
2024-03-16 16:01:03 +0530

    url changed

--- apps/studio-next/src/app/layout.tsx
+++ apps/studio-next/src/app/layout.tsx
@@ -7,9 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Test Heading ',
-  description: 'Test description. This will check the length of the description.',
-  // Add og:image URL here
-  image: 'https://example.com/path/to/your/image.jpg',
+  description: 'Test description . This will check the length of the description.',
 }
 
 export default function RootLayout({
@@ -19,13 +17,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        {/* Add meta tags for social media sharing */}
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.image} />
-        {/* You can add more meta tags as needed */}
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
