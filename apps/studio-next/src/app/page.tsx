0dba4da0b7cb11fd55608f30774087a66871293b
Ankit Dash <ankitdash2019@gmail.com>
2024-05-28 22:11:04 +0530

    fixes

--- apps/studio-next/src/app/page.tsx
+++ apps/studio-next/src/app/page.tsx
@@ -1,5 +1,25 @@
 import dynamic from 'next/dynamic';
 const StudioWrapper = dynamic(() => import('@/components/StudioWrapper'), {ssr: false})
+import { Metadata } from 'next';
+import ogImage from '@/app/meta-studio-og-image.jpeg';
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://studio-helios2003.netlify.app/'),
+  openGraph: {
+    type: 'website',
+    title: 'AsyncAPI Studio',
+    description: 'Studio for AsyncAPI specification, where you can validate, view preview documentation, and generate templates from AsyncAPI document.',
+    url: 'https://studio-helios2003.netlify.app/',
+    images: [
+      {
+        url: ogImage.src,
+        width: 800,
+        height: 600,
+        alt: 'AsyncAPI default image',
+      },
+    ]
+  }
+}
 export default async function Home() {
   return (
     <StudioWrapper />
