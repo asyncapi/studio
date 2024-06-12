58934c94cef4c90be394eb5fa8494d9b34fed1ed
Khuda Dad Nomani <khudadad414@gmail.com>
2024-06-12 13:13:29 +0100

    remove scroll

--- packages/ui/components/DropdownChipInput.tsx
+++ packages/ui/components/DropdownChipInput.tsx
@@ -1,6 +1,5 @@
 
 import { FunctionComponent, KeyboardEvent, useRef } from 'react';
-import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu"
 import { Chip } from './Chip';
 import {
   DropdownMenu,
@@ -11,7 +10,6 @@ import {
 import { PlusCircleIcon } from './icons';
 import { cn } from '@asyncapi/studio-utils';
 
-type Checked = DropdownMenuCheckboxItemProps["checked"]
 
 interface DropdownChipInputProps {
   className?: string;
@@ -56,12 +54,10 @@ export const DropdownChipInput: FunctionComponent<DropdownChipInputProps> = ({
 
   return (
     <div className={cn("flex h-11 p-1 bg-gray-900 rounded border border-gray-800 items-center",isDisabled && "opacity-50 pointer-events-none", className)}>
-      <div className='flex overflow-scroll no-scrollbar'>
       {chips.length === 0 && placeholder && <span className='text-gray-500'>{placeholder}</span>}
       {chips.map((chip, index) => (
         <Chip key={chip} chip={chip} tabIndex={0} onDelete={handleDelete} onKeyDown={handleChipKeyDown(index)} ref={index === 0 ? firstChipRef : undefined} />
       ))}
-      </div>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
       <PlusCircleIcon className='min-w-7 w-7 h-7 min-h-7 text-gray-500 ml-auto ' />
