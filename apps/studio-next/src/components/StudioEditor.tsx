209e9d3bef0ca3305de0088e4a3a2dec24af90ba
jerensl <54782057+jerensl@users.noreply.github.com>
2024-07-05 17:34:11 +0800

    fix(cold-start): add description about the issue

--- apps/studio-next/src/components/StudioEditor.tsx
+++ apps/studio-next/src/components/StudioEditor.tsx
@@ -2,7 +2,14 @@
 import dynamic from 'next/dynamic'
 const StudioWrapper = dynamic(() => import('@/components/StudioWrapper'), {ssr: false})
 
+/*
+Calling next/dynamic with client component in server component is so far not able to do code splitting. 
+So we move them to a here as client component and call next/dynamic here to enable code splitting.
 
+Using next/dynamic in server components will also be including in server side rendering 
+which will contribute to cold start and huge bundle size.
+https://github.com/vercel/next.js/issues/49454#issuecomment-1830053413
+*/
 export default async function StudioEditor() {
   return (
     <StudioWrapper />
