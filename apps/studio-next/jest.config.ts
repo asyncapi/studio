ce5d90642d73c3e671386dd477c4d3d0f4f81973
Ankit Dash <ankitdash2019@gmail.com>
2024-09-09 22:48:40 +0530

    set up configuration for tests

--- apps/studio-next/jest.config.ts
+++ apps/studio-next/jest.config.ts
@@ -1,6 +1,23 @@
-module.exports = {
-    preset: 'ts-jest',
-    transform: {
-      '^.+\\.(ts|tsx)?$': 'ts-jest',
-    }
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest();
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.yml$': 'jest-transform-yaml',
+    '^.+\\.yaml$': 'jest-transform-yaml',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
 };
+
+const asyncConfig = createJestConfig(config);
+
+module.exports = async () => {
+  const config = await asyncConfig();
+  config.transformIgnorePatterns = ['node_modules/.pnpm/(?!@asyncapi/react-component|monaco-editor)/'];
+  return config;
+}
