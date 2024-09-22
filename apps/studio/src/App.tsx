fd7029436fae692a285965176398fd540b39fe14
catosaurusrex2003 <mohdmehdi2003@gmail.com>
2024-09-22 14:49:23 +0530

    deleted studio folder

--- apps/studio/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { AsyncAPIStudio } from './studio';
-
-import type { FunctionComponent } from 'react';
-
-export const App: FunctionComponent = () => {
-  return (
-    <AsyncAPIStudio />
-  );
-};
--- apps/studio/src/components/Visualiser/utils/random-background-color.ts
+++
@@ -1,7 +0,0 @@
-export default (stringInput: string) => {
-  //@ts-ignore
-  const stringUniqueHash = [...stringInput].reduce((acc, char) => {
-    return char.charCodeAt(0) + ((acc << 5) - acc);
-  }, 0);
-  return `hsla(${stringUniqueHash % 360}, 95%, 35%, 0.5)`;
-};
