0428e0f7a6a03dde2fbe53b6a4f9f34f40aef2e3
catosaurusrex2003 <mohdmehdi2003@gmail.com>
2024-10-01 22:25:35 +0530

    changed-urls-and-defaultport

--- apps/studio/cypress.config.ts
+++ apps/studio/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3001',
+    baseUrl: 'http://localhost:3000',
     retries: {
       runMode: 1,
       openMode: 1,
