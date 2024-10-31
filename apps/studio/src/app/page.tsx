e332a8adaa5ea93562d0532cc1c9adc019b99933
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2024-10-31 17:02:42 +0530

    chore: add title and other metadata (#1151)

--- apps/studio/src/app/page.tsx
+++ apps/studio/src/app/page.tsx
@@ -1,28 +1,44 @@
 import dynamic from 'next/dynamic';
 const StudioWrapper = dynamic(() => import('@/components/StudioWrapper'), {ssr: false})
 import { Metadata } from 'next';
-import ogImage from '@/img/meta-studio-og-image.jpeg';
 
-export const metadata: Metadata = {
-  metadataBase: new URL('https://studio.netlify.app'),
+const description = 'Studio for AsyncAPI specification, where you can validate, view preview documentation, and generate templates from AsyncAPI document.';
+const title = 'AsyncAPI Studio';
+const url = 'https://studio.asyncapi.com/';
+
+export const metadata : Metadata = {
+  title,
+  description,
+  metadataBase: new URL(url),
+  themeColor: '#000000',
   openGraph: {
     type: 'website',
-    title: 'AsyncAPI Studio',
-    description: 'Studio for AsyncAPI specification, where you can validate, view preview documentation, and generate templates from AsyncAPI document.',
-    url: 'https://studio.netlify.app',
+    url,
+    title,
+    description,
     images: [
       {
-        url: ogImage.src,
-        width: 800,
-        height: 600,
-        alt: 'AsyncAPI default image',
+        url: '/img/meta-studio-og-image.jpeg',
+        width: 1200,
+        height: 630,
+        alt: 'AsyncAPI Studio preview',
       },
-    ]
+    ],
   },
   twitter: {
+    card: 'summary_large_image',
     site: '@AsyncAPISpec',
-  }
-}
+    title,
+    description:
+      'Studio for AsyncAPI specification, where you can validate, view preview documentation, and generate templates from AsyncAPI document.',
+    images: ['/img/meta-studio-og-image.jpeg'],
+  },
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/favicon-194x194.png',
+  },
+};
+
 export default async function Home() {
   return (
     <StudioWrapper />
