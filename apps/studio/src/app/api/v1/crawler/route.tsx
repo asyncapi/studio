e87ce692814b9d099e5f873980d9a226b61c25d2
Ankit Dash <ankitdash2019@gmail.com>
2024-11-13 21:35:24 +0530

    update according to latest version of studio

---
+++ apps/studio/src/app/api/v1/crawler/route.tsx
@@ -0,0 +1,103 @@
+import { NextRequest, NextResponse } from "next/server";
+import parseURL from "@/helpers/parser";
+import { DocumentInfo } from "@/types";
+import axios from "axios";
+import { metadata } from "@/app/page";
+
+export async function GET(request: NextRequest) {
+    const Base64searchParams = request.nextUrl.searchParams.get('base64');
+    const URLsearchParams = request.nextUrl.searchParams.get('url');
+
+    try {
+        if (!Base64searchParams && !URLsearchParams) return new NextResponse(null, { status: 200 });
+        let info: DocumentInfo | null = null;
+
+        if (Base64searchParams) {
+            // directly run the parsing function
+            info = await parseURL(Base64searchParams);
+        }
+        if (URLsearchParams) {
+            // fetch the document information from the URL
+            try {
+                const response = await axios.get(URLsearchParams);
+                if (response.status === 200) {
+                    info = await parseURL(response.data);
+                } else {
+                    return new NextResponse("Not a valid URL", { status: 500 });
+                }
+            } catch (error) {
+                return new NextResponse("Not a valid URL", { status: 500 });
+            }
+        }
+
+        if (!info) {
+            const ogImage = "https://raw.githubusercontent.com/asyncapi/studio/master/apps/studio-next/public/img/meta-studio-og-image.jpeg";
+
+            const crawlerInfo = `
+       <!DOCTYPE html>
+       <html lang="en">
+       <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>"${metadata.openGraph?.title}"</title>
+        <meta property="og:title" content="${metadata.openGraph?.title}" />
+        <meta property="og:description" content="${metadata.openGraph?.description}" />
+        <meta property="og:url" content="${metadata.openGraph?.url}" />
+        <meta property="og:image" content="${ogImage}" />
+      `
+            return new NextResponse(crawlerInfo, {
+                headers: {
+                    'Content-Type': 'text/html',
+                },
+            })
+        }
+
+        let ogImageParams = new URLSearchParams();
+
+        if (info.title) {
+            ogImageParams.append('title', info.title.toString());
+        }
+        if (info.description) {
+            ogImageParams.append('description', info.description.toString());
+        }
+        if (info.numServers) {
+            ogImageParams.append('numServers', info.numServers.toString());
+        }
+        if (info.numChannels) {
+            ogImageParams.append('numChannels', info.numChannels.toString());
+        }
+        if (info.numOperations) {
+            ogImageParams.append('numOperations', info.numOperations.toString());
+        }
+        if (info.numMessages) {
+            ogImageParams.append('numMessages', info.numMessages.toString());
+        }
+
+        const ogImageurl = `https://ogp-studio.vercel.app/api/og?${ogImageParams.toString()}`;
+
+        const crawlerInfo = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>${info.title}</title>
+        ${info.title ? `<meta property="og:title" content="${info.title}" />` : ''}
+        ${info.description ? `<meta property="og:description" content="${info.description}" />` : ''}
+        <meta property="og:image" content=${ogImageurl} />
+      </head>
+      </html>
+    `;
+        console.log(crawlerInfo);
+        return new NextResponse(crawlerInfo, {
+            status: 200,
+            headers: {
+                'Content-Type': 'text/html',
+            },
+        });
+    } catch (err) {
+        return new NextResponse("Not a valid URL", { status: 500 });
+    }
+}
---
+++ apps/studio/src/helpers/tests/og.test.ts
@@ -0,0 +1,64 @@
+/**
+ * @jest-environment node
+ */
+
+import { fetchOpenGraphTags } from './fetchogtags';
+
+// list of sample crawlers to test with
+const facebookCrawler = 'facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)';
+const XCrawler = 'Twitterbot/1.0';
+const SlackCrawler = 'Slackbot-LinkExpanding 1.0 (+https://api.slack.com/robots)';
+
+// Testing with base64 query param
+const base64url = "https://studio-next.netlify.app/?base64=YXN5bmNhcGk6IDMuMC4wCmluZm86CiAgdGl0bGU6IEFjY291bnQgU2VydmljZQogIHZlcnNpb246IDEuMC4wCiAgZGVzY3JpcHRpb246IFRoaXMgc2VydmljZSBpcyBpbiBjaGFyZ2Ugb2YgcHJvY2Vzc2luZyB1c2VyIHNpZ251cHMKY2hhbm5lbHM6CiAgdXNlclNpZ25lZHVwOgogICAgYWRkcmVzczogdXNlci9zaWduZWR1cAogICAgbWVzc2FnZXM6CiAgICAgIFVzZXJTaWduZWRVcDoKICAgICAgICAkcmVmOiAnIy9jb21wb25lbnRzL21lc3NhZ2VzL1VzZXJTaWduZWRVcCcKb3BlcmF0aW9uczoKICBzZW5kVXNlclNpZ25lZHVwOgogICAgYWN0aW9uOiBzZW5kCiAgICBjaGFubmVsOgogICAgICAkcmVmOiAnIy9jaGFubmVscy91c2VyU2lnbmVkdXAnCiAgICBtZXNzYWdlczoKICAgICAgLSAkcmVmOiAnIy9jaGFubmVscy91c2VyU2lnbmVkdXAvbWVzc2FnZXMvVXNlclNpZ25lZFVwJwpjb21wb25lbnRzOgogIG1lc3NhZ2VzOgogICAgVXNlclNpZ25lZFVwOgogICAgICBwYXlsb2FkOgogICAgICAgIHR5cGU6IG9iamVjdAogICAgICAgIHByb3BlcnRpZXM6CiAgICAgICAgICBkaXNwbGF5TmFtZToKICAgICAgICAgICAgdHlwZTogc3RyaW5nCiAgICAgICAgICAgIGRlc2NyaXB0aW9uOiBOYW1lIG9mIHRoZSB1c2VyCiAgICAgICAgICBlbWFpbDoKICAgICAgICAgICAgdHlwZTogc3RyaW5nCiAgICAgICAgICAgIGZvcm1hdDogZW1haWwKICAgICAgICAgICAgZGVzY3JpcHRpb246IEVtYWlsIG9mIHRoZSB1c2Vy";
+const accountServiceTags = {
+    "og:title": 'Account Service',
+    "og:description": 'This service is in charge of processing user signups',
+    "og:image": 'https://ogp-studio.vercel.app/api/og?title=Account+Service&description=This+service+is+in+charge+of+processing+user+signups&numChannels=1&numOperations=1&numMessages=1'
+}
+
+describe('Testing the document with base64 query parameter for various open graph crawlers', () => {
+    jest.setTimeout(30000);
+
+    test('Test Open Graph tags for Facebook', async () => {
+        const openGraphTags = await fetchOpenGraphTags(base64url, facebookCrawler);
+        expect(openGraphTags).equal(accountServiceTags);
+    });
+
+    test('Test Open Graph tags for X', async () => {
+        const openGraphTags = await fetchOpenGraphTags(base64url, XCrawler);
+        expect(openGraphTags).equal(accountServiceTags);
+    });
+
+    test('Test Open Graph tags for Slack', async () => {
+        const openGraphTags = await fetchOpenGraphTags(base64url, SlackCrawler);
+        expect(openGraphTags).equal(accountServiceTags);
+    });
+})
+
+// Testing with url query param
+const externalDocUrl = 'https://studio-next.netlify.app/?url=https://raw.githubusercontent.com/asyncapi/spec/master/examples/mercure-asyncapi.yml';
+const mercurHubTags = {
+    "og:title": 'Mercure Hub Example',
+    "og:description": 'This example demonstrates how to define a Mercure hub.',
+    "og:image": 'https://ogp-studio.vercel.app/api/og?title=Mercure+Hub+Example&description=This+example+demonstrates+how+to+define+a+Mercure+hub.&numServers=1&numChannels=1&numOperations=2&numMessages=1'
+}
+
+describe('Testing the document with url query parameter for various open graph crawlers', () => {
+    jest.setTimeout(30000);
+
+    test('Test Open Graph tags for Facebook', async () => {
+        const openGraphTags = await fetchOpenGraphTags(externalDocUrl, facebookCrawler);
+        expect(openGraphTags).equal(mercurHubTags);
+    });
+
+    test('Test Open Graph tags for X', async () => {
+        const openGraphTags = await fetchOpenGraphTags(externalDocUrl, XCrawler);
+        expect(openGraphTags).equal(mercurHubTags);
+    });
+
+    test('Test Open Graph tags for Slack', async () => {
+        const openGraphTags = await fetchOpenGraphTags(externalDocUrl, SlackCrawler);
+        expect(openGraphTags).equal(mercurHubTags);
+    });
+})
