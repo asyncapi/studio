ca01b8c0676cbc5a721c62b84cb7a83c714577a2
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2026-03-06 11:43:04 +0100

    fix for netlify build error

--- apps/studio/src/helpers/file-system-access.types.ts
+++ apps/studio/src/helpers/file-system-access.types.ts
@@ -1,2 +1,14 @@
-export type DirectoryHandle = Awaited<ReturnType<Window['showDirectoryPicker']>>;
-export type FileHandle = Awaited<ReturnType<Window['showOpenFilePicker']>>[number];
+export interface FileHandle {
+  kind: 'file';
+  name: string;
+  getFile: () => Promise<File>;
+}
+
+export interface DirectoryHandle {
+  kind: 'directory';
+  name: string;
+  entries: () => AsyncIterableIterator<[string, DirectoryHandle | FileHandle]>;
+  getDirectoryHandle: (name: string) => Promise<DirectoryHandle>;
+  getFileHandle: (name: string) => Promise<FileHandle>;
+  resolve: (possibleDescendant: FileHandle) => Promise<string[] | null>;
+}
