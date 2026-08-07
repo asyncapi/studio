85ee769c8549fd6153112d4d95484308a32d0031
Shurtu-gal <ashishpadhy1729@gmail.com>
2026-08-07 11:52:27 +0530

    feat: add new asyncapi design system icons
    Signed-off-by: Shurtu-gal <ashishpadhy1729@gmail.com>

---
+++ packages/ui/components/icons/SaveIcon.tsx
@@ -0,0 +1,13 @@
+const SaveIcon = (props: any) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" {...props}>
+    <g transform="translate(1.11628) scale(0.55814)">
+      <path d="M1.5 21.2169C1.5 12.064 1.5 7.48752 4.27347 4.58789C4.37593 4.48077 4.48077 4.37593 4.58789 4.27347C7.48752 1.5 12.064 1.5 21.2169 1.5C23.386 1.5 24.431 1.50751 25.4256 1.87873C26.3834 2.23619 27.1857 2.90479 28.7904 4.24197L33.1822 7.90185C35.3055 9.67121 36.3671 10.5559 36.9335 11.7653C37.5 12.9747 37.5 14.3567 37.5 17.1205V23.5C37.5 30.9995 37.5 34.7492 35.5902 37.3779C34.9734 38.2268 34.2268 38.9734 33.3779 39.5902C30.7492 41.5 26.9995 41.5 19.5 41.5C12.0005 41.5 8.2508 41.5 5.62215 39.5902C4.7732 38.9734 4.02663 38.2268 3.40983 37.3779C1.5 34.7492 1.5 30.9995 1.5 23.5V21.2169Z" stroke="currentColor" strokeWidth="1.5" vectorEffect="non-scaling-stroke" />
+      <path d="M11.5 41.5V35.5C11.5 31.7288 11.5 29.8431 12.6716 28.6716C13.8431 27.5 15.7288 27.5 19.5 27.5C23.2712 27.5 25.1569 27.5 26.3284 28.6716C27.5 29.8431 27.5 31.7288 27.5 35.5V41.5" stroke="currentColor" strokeWidth="1.5" vectorEffect="non-scaling-stroke" strokeLinejoin="round" />
+      <path d="M15.5 11.5H23.5" stroke="currentColor" strokeWidth="1.5" vectorEffect="non-scaling-stroke" strokeLinecap="round" strokeLinejoin="round" />
+    </g>
+  </svg>
+)
+
+SaveIcon.displayName = 'SaveIcon'
+
+export default SaveIcon
